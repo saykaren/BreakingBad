f1a068f31948cb06d8f1d7b25f7cf153eeeaee98
Karen Garcia <saykaren@gmail.com>
2021-05-26 15:13:54 -0600

    Add React-Query

---
+++ src/Components/App/App.js
@@ -0,0 +1,20 @@
+import "./../Styling/App.scss";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import Main from './Main';
+
+const queryClient = new QueryClient();
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <header className="App-header">Hello</header>
+        <Main />
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} position={"bottom-right"} />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
---
+++ src/Components/useQuery/fetchUrl.js
@@ -0,0 +1,9 @@
+const fetchURL = async ({ queryKey }) => {
+    const [, baseURL] = queryKey;
+  
+    const res = await fetch(`${baseURL}`);
+    return res.json();
+  };
+  
+  export default fetchURL;
+  
