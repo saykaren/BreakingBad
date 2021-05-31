0c61d87a29ef2bf217c20de2b7de98fc2d549198
Karen Garcia <saykaren@gmail.com>
2021-05-31 12:08:57 -0600

    Adds Jest and Enzyme Testing Still failing on Table not understanding why it fails on data as I even put the Mock_Data into the app itself and it worked

---
+++ src/Components/Testing/App.test.js
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import App from "./../App/App";
+import React from "react";
+import Table from "./../App/Table";
+import Main from "./../App/Main";
+import { shallow } from "enzyme";
+
+describe("rendering components", () => {
+  it("renders App component without crashing", () => {
+    shallow(<App />);
+  });
+  it("render App component header without crashing", () => {
+    const wrapper = shallow(<App />);
+    const mainComponent = (
+      <div className="App">
+        <Main />
+      </div>
+    );
+    expect(wrapper.contains(mainComponent)).toEqual(true);
+  });
+});
