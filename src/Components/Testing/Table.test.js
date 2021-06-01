1a3e6a14dbc8851ce997cbaef0c57198ec912807
Karen Garcia <saykaren@gmail.com>
2021-05-31 21:14:13 -0600

    Adds Additional Testing onto Table

--- src/Components/Testing/Table.test.js
+++ src/Components/Testing/Table.test.js
@@ -1,23 +1,34 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
 import Table from "./../App/Table";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import Mock_Data from "./Mock_Data";
+import fetchURL from "../useQuery/fetchUrl";
 
-describe("rendering components", () => {
-  it("renders Table component without crashing", () => {
-    shallow(<Table />);
+const dataHere = fetchURL("https://breakingbadapi.com/api/characters");
+
+describe("Render Table", () => {
+  const wrapper = shallow(<Table dataSet={Mock_Data} />);
+  const table = wrapper.find("table");
+  const row = table.find("tr");
+  const node = table.find("td");
+  const modal = wrapper.find(".modal");
+
+  it("table grid", () => {
+    expect(table).toHaveLength(1);
+    expect(row).toHaveLength(64);
   });
-  it("render App component header without crashing", () => {
-    const wrapper = shallow(<Table dataSet={Mock_Data}/>);
-    const mainComponent = (
-        <button
-        disabled={!modalData}
-        onClick={() => setModal(!modal)}
-      >
-        Activate Modal
-      </button>
-    );
-    expect(wrapper.contains(mainComponent)).toEqual(true);
+
+  it("Modal", () => {
+    expect(wrapper.exists(".modalPicture")).toEqual(false);
+    expect(wrapper.exists(".modal")).toEqual(false);
+
+    expect(node.last().props().id).toEqual("Tony Dalton");
+    expect(node.at(1).props().id).toEqual("Walter White");
+  });
+
+  it("Modal Activate", () => {
+    table.find("tr").at(3).simulate("click");
+    expect(wrapper.exists(".modal")).toEqual(true);
   });
 });
